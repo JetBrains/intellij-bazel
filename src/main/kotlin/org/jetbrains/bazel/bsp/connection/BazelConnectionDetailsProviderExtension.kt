2268a31caba8c0b518431782c4c2ce7f70eeeb70
BazelSteward <bazel@jetbrains.com>
2024-01-04 03:14:43 +0000

    [Maintenance] Update bazel-bsp server dependency version to 3.1.0-20240104-2f86f1d-NIGHTLY

--- src/main/kotlin/org/jetbrains/bazel/bsp/connection/BazelConnectionDetailsProviderExtension.kt
+++ src/main/kotlin/org/jetbrains/bazel/bsp/connection/BazelConnectionDetailsProviderExtension.kt
@@ -111,7 +111,7 @@ internal class BazelConnectionDetailsProviderExtension: ConnectionDetailsProvide
       calculateNeededJars(
         org = "org.jetbrains.bsp",
         name = "bazel-bsp",
-        version = "3.1.0-20231216-ec66172-NIGHTLY",
+        version = "3.1.0-20240104-2f86f1d-NIGHTLY",
       )
         .joinToString(File.pathSeparator),
       "org.jetbrains.bsp.bazel.install.Install",
