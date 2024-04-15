4278a861a78994658494ded8374d1773979d70ca
Katarzyna Mielnik <katarzyna.mielnik@jetbrains.com>
2024-04-15 19:38:15 +0200

    cr fixes

--- src/main/kotlin/org/jetbrains/bazel/languages/starlark/services/StarlarkFormattingService.kt
+++ src/main/kotlin/org/jetbrains/bazel/languages/starlark/services/StarlarkFormattingService.kt
@@ -15,7 +15,6 @@ import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileTypes.FileTypeRegistry
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
 import com.intellij.psi.util.PsiEditorUtil
@@ -24,36 +23,29 @@ import org.jetbrains.bazel.config.BazelPluginBundle
 import org.jetbrains.bazel.languages.starlark.StarlarkFileType
 import java.io.File
 
-class StarlarkFormattingService : AsyncDocumentFormattingService() {
+private val LOG = logger<StarlarkFormattingService>()
 
-  private val LOG = logger<StarlarkFormattingService>()
+internal class StarlarkFormattingService : AsyncDocumentFormattingService() {
   override fun getFeatures(): Set<Feature> = emptySet()
 
   override fun canFormat(file: PsiFile): Boolean {
-    val virtualFile: VirtualFile = file.virtualFile ?: return false
+    val virtualFile = file.virtualFile ?: return false
     return FileTypeRegistry.getInstance().isFileOfType(virtualFile, StarlarkFileType)
   }
 
   override fun createFormattingTask(request: AsyncFormattingRequest): FormattingTask? {
+    //TODO: buildifier settings (https://youtrack.jetbrains.com/issue/BAZEL-927/settings-page-for-buildifier)
     val buildifierPath = "buildifier"
-    val ioFile: File = request.ioFile ?: return null
     val psiFile = request.context.containingFile
     val project = request.context.project
     val document = PsiDocumentManager.getInstance(project).getDocument(psiFile)
-
 
     if (document == null) {
       LOG.warn("Document for file ${psiFile.name} is null")
       return null
     }
 
-    val commandLine: GeneralCommandLine = GeneralCommandLine()
-      .withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
-      .withExePath(buildifierPath)
-      .withInput(ioFile)
-      .withWorkDirectory(ioFile.parent)
-
-    val handler = CapturingProcessHandler(commandLine)
+    val handler = createProcessHandler(request, buildifierPath) ?: return null
 
     return object : FormattingTask {
       override fun run() {
@@ -71,42 +63,55 @@ class StarlarkFormattingService : AsyncDocumentFormattingService() {
         return true
       }
 
-      override fun isRunUnderProgress(): Boolean {
-        return true
-      }
+      override fun isRunUnderProgress(): Boolean = true
     }
+  }
+
+  private fun createProcessHandler(request: AsyncFormattingRequest, buildifierPath: String): CapturingProcessHandler? {
+    val ioFile: File = request.ioFile ?: return null
+    val commandLine: GeneralCommandLine = GeneralCommandLine()
+      .withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
+      .withExePath(buildifierPath)
+      .withInput(ioFile)
+      .withWorkDirectory(ioFile.parent)
+
+    return CapturingProcessHandler(commandLine)
   }
 
   override fun getNotificationGroupId(): String = NOTIFICATION_GROUP_ID
 
-  override fun getName(): String = "Starlark file"
+  override fun getName(): String = BazelPluginBundle.message("buildifier.formatting.service.name")
 
   private companion object {
-    const val NOTIFICATION_GROUP_ID = "Buildifier"
+    private const val NOTIFICATION_GROUP_ID = "Buildifier"
+
   }
 }
 
 internal open class BuildifierProcessListener(private val request: AsyncFormattingRequest) : CapturingProcessAdapter() {
   override fun processTerminated(event: ProcessEvent) {
     val exitCode: Int = event.exitCode
-    if (exitCode == 0) {
-      if (output.stdout.isEmpty()) {
-        showFormattedLinesInfo("Ignored")
-        request.onTextReady(null)
-      } else {
-        showFormattedLinesInfo(BazelPluginBundle.message("buildifier.formatted.success"))
-        request.onTextReady(output.stdout)
-      }
-    } else {
-      request.onError(BazelPluginBundle.message("buildifier.run.error.message"), output.stderr)
+    when (exitCode) {
+      0 -> showFormattedOutput()
+      else -> request.onError(BazelPluginBundle.message("buildifier.run.error.message"), output.stderr)
     }
   }
 
-  private fun showFormattedLinesInfo(text: String, isError: Boolean = false) {
+  private fun showFormattedOutput() {
+    if (output.stdout.isEmpty()) {
+      showFormattedLinesInfo(BazelPluginBundle.message("buildifier.formatted.ignored"))
+      request.onTextReady(null)
+    } else {
+      showFormattedLinesInfo(BazelPluginBundle.message("buildifier.formatted.success"))
+      request.onTextReady(output.stdout)
+    }
+  }
+
+  private fun showFormattedLinesInfo(text: String) {
     PsiEditorUtil.findEditor(request.context.containingFile)?.let { editor: Editor ->
       ApplicationManager.getApplication()
         .invokeLater({
-          val component = if (isError) HintUtil.createErrorLabel(text) else HintUtil.createInformationLabel(text)
+          val component = HintUtil.createInformationLabel(text)
           val hint = LightweightHint(component)
           HintManagerImpl.getInstanceImpl()
             .showEditorHint(hint, editor, HintManager.ABOVE,
